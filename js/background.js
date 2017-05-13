c5a1fdc371b8f684166a24de41cf21f04f8b8852
Camille Nibungco <camille.nibungco@gmail.com>
2017-05-13 04:52:10 -0700

    updated style mainly

--- js/background.js
+++ js/background.js
@@ -3,3 +3,8 @@ chrome.browserAction.onClicked.addListener(function(){
         chrome.tabs.sendMessage(tabs[0].id, "toggle");
     })
 })
+/*TO-DO: Professor ID Tags increment as an array starting from $0
+/ MTG_INSTR$0, MTG_INSTR$1, MTG_INSTR$2, etc.
+*/
+var prof = document.getElementById("MTG_INSTR$0").innerText;
+console.log(prof);
